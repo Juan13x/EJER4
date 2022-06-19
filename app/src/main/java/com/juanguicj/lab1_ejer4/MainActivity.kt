c2683c7d59220beb344b343acd53f0292334c44b
Juan <juang15x@gmail.com>
2022-06-18 20:49:47 -0500

    version 1.0

--- app/src/main/java/com/juanguicj/lab1_ejer4/MainActivity.kt
+++ app/src/main/java/com/juanguicj/lab1_ejer4/MainActivity.kt
@@ -14,30 +14,28 @@ class MainActivity : AppCompatActivity() {
         val view = mainBinding.root
         setContentView(view)
 
-        with(mainBinding){
+        with(mainBinding) {
             resolveButton.setOnClickListener {
                 val idBand1 = band1Spinner.selectedItemPosition
                 val idBand2 = band2Spinner.selectedItemPosition
                 val idBand3 = multiplierSpinner.selectedItemPosition
                 val idBand4 = toleranceSpinner.selectedItemPosition
 
-                var result = 0.0f
                 var tolerance = ""
                 var accumulator = ""
 
                 accumulator += idBand1.toString()
                 accumulator += idBand2.toString()
-                result = accumulator.toFloat()
-                if(idBand3 < 10){
-                    result *= 10.0f.pow(idBand3)
-                }
-                else{
-                    when (idBand3){
-                        10 -> result *= 0.1f
-                        else -> result *= 0.01f
+                var result = accumulator.toFloat()
+                result *= if (idBand3 < 10) {
+                    10.0f.pow(idBand3)
+                } else {
+                    when (idBand3) {
+                        10 -> 0.1f
+                        else -> 0.01f
                     }
                 }
-                when (idBand4){
+                when (idBand4) {
                     0 -> tolerance = " ± 1%"
                     1 -> tolerance = " ± 2%"
                     2 -> tolerance = " ± 0.5%"
@@ -47,7 +45,8 @@ class MainActivity : AppCompatActivity() {
                     6 -> tolerance = " ± 5%"
                     7 -> tolerance = " ± 10%"
                 }
-                resultTextView.text = result.toString() + tolerance
+                val resultSTR = result.toString() + tolerance
+                resultTextView.text = resultSTR
             }
         }
     }
